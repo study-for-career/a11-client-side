6278c0ca865a66c5bd7bfd4daca9fb7933dbe3b7
Md. Zahidul Islam <mdzahidulislam.bera@gmail.com>
2025-01-04 20:49:24 +0600

    Add service functionality Added

--- src/private pages/AddService.jsx
+++ src/private pages/AddService.jsx
@@ -23,7 +23,7 @@ const AddService = () => {
             position: "top-center"
         })
     }
-
+    // Add service function
     const handleAddService = e => {
         e.preventDefault()
 
@@ -43,7 +43,7 @@ const AddService = () => {
             })
     }
 
-// Add Service Form 
+    // Add Service Form 
     return (
         <div className="py-10 px-2 md:px-16">
 
